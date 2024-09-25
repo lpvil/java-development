731e312876b88dfc5dc40dd4bedca6f495ff92b1
lpvil <lpvillalone@gmail.com>
2024-09-25 13:12:44 -0700

    maven

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("apple");
+        System.out.println("juice");
+        System.out.println("pizza");
+        System.out.println("gatorade");
+        System.out.println("milk");
+        System.out.println("eggs");
+        System.out.println("steak");
+        System.out.println("bananas");
+        System.out.println("pineapple");
+        System.out.println("bread");
+    }
+}
